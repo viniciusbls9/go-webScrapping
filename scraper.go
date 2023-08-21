e50f23a8fded667a3ce1f3cd9656f07e50885872
viniciusbls9 <beneditoloura@gmail.com>
2023-08-21 07:16:49 -0300

    feat: add FromPrice variable to get discount if exists

--- scraper.go
+++ scraper.go
@@ -11,7 +11,8 @@ import (
 
 type item struct {
 	Name 				string `json:"name"`
-	Price 			string `json:"price"`
+	FromPrice		string `json:"fromPrice"`
+	ToPrice			string `json:"toPrice"`
 	ImgUrl 			string `json:"imgurl"`
 	ProductUrl 	string `json:"producturl"`
 }
@@ -27,7 +28,8 @@ func scrapper(responseWritter http.ResponseWriter, request *http.Request) {
 	c.OnHTML("div.ui-search-result--core", func(h *colly.HTMLElement) {
 		item := item{
 			Name: h.ChildText("h2.ui-search-item__title"),
-			Price: h.ChildText("div.ui-search-item__group__element span.andes-money-amount"),
+			FromPrice: h.ChildText(".ui-search-price__original-value .andes-money-amount__fraction"),
+			ToPrice: h.ChildText("span.ui-search-price__part--medium span.andes-money-amount__fraction"),
 			ImgUrl: h.ChildAttr("img", "src"),
 			ProductUrl: h.ChildAttr("a.ui-search-link", "href"),
 		}
